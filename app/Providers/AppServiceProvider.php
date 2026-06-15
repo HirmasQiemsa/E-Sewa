e5e35116a5f5fcc3277b2657254cd14c534105f4
Hirmas <hirmasn9@gmail.com>
2026-06-15 21:55:21 +0700

    hapus Sqlite pragma & deleted authentication-card-logo

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -41,9 +41,9 @@ class AppServiceProvider extends ServiceProvider
         ]);
 
         // ✅ foreign key constraint khusus untuk SQLite
-        if (DB::getDriverName() === 'sqlite') {
-        DB::statement('PRAGMA foreign_keys = ON;');
-        }
+        // if (DB::getDriverName() === 'sqlite') {
+        // DB::statement('PRAGMA foreign_keys = ON;');
+        // }
 
         // Daftarkan middleware role Versi Lama
         // $this->app->router->aliasMiddleware('admin', AdminMiddleware::class);
